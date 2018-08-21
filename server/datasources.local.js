8fc7b3cff6eb01289dd138268d7beb94ce6d00a5
Sambi Udumula <sambireddy4java@gmail.com>
2018-08-21 11:28:06 +0530

    Assets Models

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,18 +1,18 @@
 'use strict';
-//const elasticSchema = require('./elasticMapping.json');
+const elasticSchema = require('./elasticMapping.json');
 const elasticConfig = require('../config/config.local.json').elastic;
 module.exports = {
   'db': {
     'name': 'db',
     'connector': 'memory'
   },
-  'dmOrganizationDB': {
-    'name': 'dmOrganizationDB',
+  'es': {
+    'name': 'es',
     'connector': 'esv6',
-    'index': elasticConfig.dmOrganizationIndex,
+    'index': elasticConfig.index,
     'hosts': elasticConfig.hosts,
     'mappingType': 'basedata',
-    //'mappingProperties': elasticSchema.dmOrgmappingProperties,
+    'mappingProperties': elasticSchema.mappingProperties,
     'mappings': [],
     'defaultSize': 1000,
     'log': false,
