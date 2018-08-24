94e1f3da7f7dc400c3696d1e0527f99861b15869
Madhu Salapu <madhu.salapu@macys.com>
2018-08-24 15:22:14 +0530

    Added notification model

--- server/datasources.dev.js
+++ server/datasources.dev.js
@@ -45,11 +45,23 @@ module.exports = {
     'apiVersion': '6.0',
     'requestTimeout': 30000
   },
-
   'resource': {
     'name': 'resource',
     'connector': 'esv6',
     'index': elasticConfig.resourceIndex,
+    'hosts': elasticConfig.hosts,
+    'mappingType': 'basedata',
+    //'mappingProperties': elasticSchema.dmOrgmappingProperties,
+    'mappings': [],
+    'defaultSize': 1000,
+    'log': false,
+    'apiVersion': '6.0',
+    'requestTimeout': 30000
+  },
+  'notificationDB': {
+    'name': 'resource',
+    'connector': 'esv6',
+    'index': elasticConfig.notificationIndex,
     'hosts': elasticConfig.hosts,
     'mappingType': 'basedata',
     //'mappingProperties': elasticSchema.dmOrgmappingProperties,
